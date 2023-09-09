fa23a15e4e400a590389edb8f5d2d0b2e5efb3c0
hanseungyeoun <aksla1122@naver.com>
2023-09-09 23:53:13 +0900

    ecr

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "eks" {
   # eks 모듈에서 사용할 변수 정의
-  source = "./modules/eks-cluster"
-  cluster_name = "fast-cluster"
+  source          = "./modules/eks-cluster"
+  cluster_name    = "fast-cluster"
   cluster_version = "1.24"
-  vpc_id = "vpc-01c207337455f056d"
+  vpc_id          = "vpc-0d1220c8289d60d3d"
 
-  private_subnets = ["subnet-09567775f0c70b374", "subnet-06e1632299878e7ac"]
-  public_subnets  = ["subnet-028ffbc29178cd92a", "subnet-0f789acfcc010432b"]
+  private_subnets = ["subnet-0c87314552d75bd88", "subnet-05e9d51fd644b0bf6"]
+  public_subnets  = ["subnet-0b0b6cd970b9fc17b", "subnet-0d91bcf99ec9d602e"]
 }
